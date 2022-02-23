03aedc71607ddbc8974d5ce525dbbcfa013d176e
yoojiw <yoojiw@sheridancollege.ca>
2022-02-23 05:43:40 -0500

    added new class PartTimeStudentvv

---
+++ src/javapractice/addClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author irene
+ */
+public class addClass {
+    //I made a new bracn, and I added up this new ckass in it. 
+}
